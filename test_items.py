7280d095b1ac6aa01345f82b5359397c5f6f75f9
Yevgeniy375811135 <zvonarev_yevgeniy@mail.ru>
2023-02-20 13:19:08 +0300

    Autotest language interface

--- test_items.py
+++ test_items.py
@@ -3,9 +3,9 @@ import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
-def test_link(browser):
-    browser.implicitly_wait(10)
+def test_button(browser):
     browser.get(link)
+    time.sleep(30)
     button_is_present = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert button_is_present, f"Button is not pesent on the page = {button_is_present}"
-    time.sleep(15)
+    
