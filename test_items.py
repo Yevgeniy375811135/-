02a3fbbd0a49fd52b40ad034908d4e91f386876e
Yevgeniy375811135 <zvonarev_yevgeniy@mail.ru>
2023-02-20 13:25:14 +0300

    Autotest

--- test_items.py
+++ test_items.py
@@ -7,5 +7,5 @@ def test_button(browser):
     browser.get(link)
     time.sleep(30)
     button_is_present = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
-    assert button_is_present, f"Button is not pesent on the page = {button_is_present}"
+    assert button_is_present, f"Button is not pesent on the page "
     
